19fb1909c73f54bcddcde1a8bf5f934182a860e5
Nika Iakobashvili <nika2003iakobashvili@gmail.com>
2025-01-06 15:20:58 +0400

    Add project files.

---
+++ Controllers/ProductCategoriesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Final_Project_Nika.Models;
+
+namespace Final_Project_Nika.Controllers
+{
+    public class ProductCategoriesController : Controller
+    {
+        private readonly AdventureWorksLTDbContext _context;
+
+        public ProductCategoriesController(AdventureWorksLTDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ProductCategories
+        public async Task<IActionResult> Index()
+        {
+            var adventureWorksLTDbContext = _context.ProductCategories
+                .Include(p => p.ParentProductCategory);
+
+
+            return View(await adventureWorksLTDbContext.ToListAsync());
+        }
+
+        // GET: ProductCategories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productCategory = await _context.ProductCategories
+                .Include(p => p.ParentProductCategory)
+                .FirstOrDefaultAsync(m => m.ProductCategoryId == id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(productCategory);
+        }
+
+        // GET: ProductCategories/Create
+        public IActionResult Create()
+        {
+            ViewData["ParentProductCategoryId"] = new SelectList(_context.ProductCategories, "ProductCategoryId", "Name");
+            return View();
+        }
+
+        // POST: ProductCategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductCategoryId,ParentProductCategoryId,Name,Rowguid,ModifiedDate")] ProductCategory productCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productCategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ParentProductCategoryId"] = new SelectList(_context.ProductCategories, "ProductCategoryId", "Name", productCategory.ParentProductCategoryId);
+            return View(productCategory);
+        }
+
+        // GET: ProductCategories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productCategory = await _context.ProductCategories.FindAsync(id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+            ViewData["ParentProductCategoryId"] = new SelectList(_context.ProductCategories, "ProductCategoryId", "Name", productCategory.ParentProductCategoryId);
+            return View(productCategory);
+        }
+
+        // POST: ProductCategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductCategoryId,ParentProductCategoryId,Name,Rowguid,ModifiedDate")] ProductCategory productCategory)
+        {
+            if (id != productCategory.ProductCategoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productCategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductCategoryExists(productCategory.ProductCategoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ParentProductCategoryId"] = new SelectList(_context.ProductCategories, "ProductCategoryId", "Name", productCategory.ParentProductCategoryId);
+            return View(productCategory);
+        }
+
+        // GET: ProductCategories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productCategory = await _context.ProductCategories
+                .Include(p => p.ParentProductCategory)
+                .FirstOrDefaultAsync(m => m.ProductCategoryId == id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(productCategory);
+        }
+
+        // POST: ProductCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var productCategory = await _context.ProductCategories.FindAsync(id);
+            if (productCategory != null)
+            {
+                _context.ProductCategories.Remove(productCategory);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductCategoryExists(int id)
+        {
+            return _context.ProductCategories.Any(e => e.ProductCategoryId == id);
+        }
+    }
+}
